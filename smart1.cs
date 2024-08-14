930429f9ef7dca5454547481936d5b962007f1af
mibali <111276665+mibali@users.noreply.github.com>
2024-08-14 12:13:00 +0100

    Create smart1.cs

---
+++ smart1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Example
+{
+    public class Greeting
+    {
+        public void SayHello(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
